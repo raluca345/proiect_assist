004aef69a152cd20e530f86406a88cd04e24838b
raluca_stamate <raluca.stamate@student.usv.ro>
2023-11-28 10:47:24 +0200

    citire din fisier

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,10 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
 import java.nio.file.FileSystems;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.Comparator;
@@ -10,7 +12,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        AngajatCuExperienta angajat1 = new AngajatCuExperienta("Popescu", "Ioan", 60);
+        /*AngajatCuExperienta angajat1 = new AngajatCuExperienta("Popescu", "Ioan", 60);
         Intern intern = new Intern("Vasilescu", "Ana", 5);
         Administrator adm = new Administrator("Georgescu", "Mihai", 130);
         System.out.println(angajat1);
@@ -23,27 +25,27 @@ public class Main {
         System.out.println(df.format(angajat1.getSalariu()));
         System.out.println(df.format(intern.getSalariu()));
         System.out.println(df.format(adm.getSalariu()));
-        System.out.println();
-        //TODO: citire din fisier in loc
+        System.out.println();*/
         //TODO: poate fa o aplicatie de tip meniu
         //TODO: error handling
         ArrayList<Angajat> lista = new ArrayList<>();
-        String[] tipuri = new String[]{"A", "A", "A", "I", "I", "M", "M"};
-        String[] nume_de_familie = new String[]{"Apetrei", "Popa", "Ilies", "Cristinescu", "Bujor", "Cojocaru", "Vacaru"};
-        String[] prenume = new String[]{"Mihai", "Mihaela", "Andreea", "Vasile", "Serban", "Ovidiu", "Sabina"};
-        int[] vechimi = new int[]{48, 57, 100, 4, 13, 140, 160};
+        String[] tipuri = new String[]{};
 
-        for(int i = 0; i < 7; i++){
-            lista.add(FabricaAngajati.getAngajat(tipuri[i], nume_de_familie[i], prenume[i], vechimi[i]));
+        try {
+            Scanner sc = new Scanner(Paths.get("angajati.txt"));
+            //citeste tipurile si creeaza angajati folosind clasa FabricaAngajati
+            tipuri = sc.nextLine().strip().split(",");
+            int i = 0;
+            while (sc.hasNextLine()){
+                String[] linie = sc.nextLine().strip().split(",");
+                lista.add(FabricaAngajati.getAngajat(tipuri[i],linie[0], linie[1], Integer.parseInt(linie[2])));
+                i++;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-
         for(Angajat a : lista){
             System.out.println(a);
-        }
-
-        lista.sort(Comparator.comparing(Angajat::getNume).thenComparing(Angajat::getPrenume));
-        for(Angajat a : lista) {
-            System.out.println(a.getNume() + " " + a.getPrenume());
         }
     }
 }
